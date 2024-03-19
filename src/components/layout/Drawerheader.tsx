223d5c0e42dedeef733d2d226b1edec9c5e92633
stp.do <46347222+sittipol1123@users.noreply.github.com>
2024-03-19 14:16:01 +0700

    ..

---
+++ src/components/layout/Drawerheader.tsx
@@ -0,0 +1,63 @@
+import "flowbite";
+
+const Drawerheader = () => {
+  return (
+    <>
+      <div
+        id="drawer-example"
+        className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800"
+      >
+        <h5
+          id="drawer-label"
+          className="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
+        >
+          ALYA SVELTEKIT SAAS
+        </h5>
+        <button
+          type="button"
+          data-drawer-hide="drawer-example"
+          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <svg
+            className="w-3 h-3"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close menu</span>
+        </button>
+        <div className="py-4 overflow-y-auto">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Drawerheader;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,114 @@
+import Drawerheader from "./Drawerheader";
+
+const Header = () => {
+  return (
+    <>
+      <Drawerheader></Drawerheader>
+      <nav
+        id="navbar"
+        className="fixed h-14 top-0 inset-x-0 z-20 w-full border-b border-gray-100 bg-white/80 backdrop-blur dark:border-gray-700/30 dark:bg-gray-900/80"
+      >
+        <div className="mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0">
+          <div className="relative flex flex-wrap items-center justify-between gap-6 lg:gap-0 lg:py-4">
+            <div className="relative z-20 flex w-full justify-between md:px-0 lg:w-max">
+              <a
+                href="/#"
+                aria-label="ampire logo"
+                className="text-xl uppercase nav-link flex items-center space-x-2"
+              >
+                Alya Sveltekit SaaS
+              </a>
+
+              <button
+                aria-label="humburger"
+                id="hamburger"
+                className="relative -mr-6 p-6 lg:hidden"
+                // onClick={() => togglenav()}
+                data-drawer-target="drawer-example"
+                data-drawer-show="drawer-example"
+                aria-controls="drawer-example"
+              >
+                <div
+                  aria-hidden="true"
+                  id="line"
+                  className="m-auto h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300"
+                ></div>
+                <div
+                  aria-hidden="true"
+                  id="line2"
+                  className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300"
+                ></div>
+              </button>
+            </div>
+
+            <div
+              aria-hidden="true"
+              id="layer"
+              className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-darker/70 lg:hidden"
+            ></div>
+            <div
+              id="navlinks"
+              className="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-auto lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none dark:lg:bg-transparent"
+            >
+              <div className="dark:text-gray-300 lg:pr-4">
+                <ul className="space-y-6 text-base tracking-wide lg:flex lg:space-y-0 lg:text-sm">
+                  <li>
+                    <a
+                      href="/#"
+                      className="nav-link block transition hover:text-primary dark:hover:text-primaryLight lg:px-4"
+                    >
+                      <span>Homepage</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/Info"
+                      className="nav-link block transition hover:text-primary dark:hover:text-primaryLight lg:px-4"
+                    >
+                      <span>Info</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href=""
+                      className="nav-link block transition hover:text-primary dark:hover:text-primaryLight lg:px-4"
+                    >
+                      <span>Product</span>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="/#features"
+                      className="nav-link block transition hover:text-primary dark:hover:text-primaryLight lg:px-4"
+                    >
+                      <span>Features</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/pricing"
+                      className="nav-link block transition hover:text-primary dark:hover:text-primaryLight lg:px-4"
+                    >
+                      <span>Pricing</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <button className="btn btn-sm variant-ghost-surface">
+                <a href="/login">
+                  <span className="relative text-sm font-semibold lg:text-primary lg:dark:text-white">
+                    Login
+                  </span>
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
