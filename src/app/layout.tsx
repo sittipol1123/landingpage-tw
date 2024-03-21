87b505be38022a3806b198c1cb661b7deddc4792
stp.do <46347222+sittipol1123@users.noreply.github.com>
2024-03-21 21:37:25 +0700

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="Sittipol Website" />
+        <meta property="og:type" content="content" />
+        <meta
+          property="og:description"
+          content="Hello, I'm Aum. I have a total of 1.8 years of experience
+                in programming. I am able to create projects and close them. I
+                have been working with cms and salepage systems for 1 year. The
+                system was developed with the laravel framework."
+        />
+        <meta property="og:image" content="/image/Roshidere.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
